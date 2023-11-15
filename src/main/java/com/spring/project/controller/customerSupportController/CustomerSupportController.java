ed2a9d7e0c75393e5ac1f153d44fd597bdc790bd
team1060 <team1060dev@gmail.com>
2023-11-15 14:33:14 +0900

    customerSupportController + View 구조 생성

--- src/main/java/com/spring/project/controller/customerSupportController/CustomerSupportController.java
+++ src/main/java/com/spring/project/controller/customerSupportController/CustomerSupportController.java
@@ -2,13 +2,28 @@ package com.spring.project.controller.customerSupportController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/support/*")
 public class CustomerSupportController {
 	
-	@GetMapping("/")
-	public @ResponseBody String index() {
-		return "hello, world";
+	// 고객 문의
+	@GetMapping("question")
+	public void question() {
+		
 	}
+	
+	// FAQ, 자주 묻는 질문
+	@GetMapping("faq")
+	public void faq() {
+		
+	}
+	
+	// 실시간 chat
+	@GetMapping("chat")
+	public void chat() {
+		
+	}
+	
 }
---
+++ src/main/java/com/spring/project/service/customerSupportService/CustomerSupportService.java
@@ -0,0 +1,5 @@
+package com.spring.project.service.customerSupportService;
+
+public class CustomerSupportService {
+
+}
