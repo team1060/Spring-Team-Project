e2f688b0702e6f635c6981b66ccb86c683ff78e7
team1060 <team1060dev@gmail.com>
2023-11-21 19:59:57 +0900

    faqComponent 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FaqComponent from './components/FaqComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <FaqComponent></FaqComponent>
     </div>
   );
 }
